3b0bb54e88e1484e3592fa17df8cb75254c758b6
havent made anything yet <86394465+luccawashere@users.noreply.github.com>
2023-09-03 15:25:45 +1000

    yy
    yes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,9 @@ export default function Index() {
     <>
       <h1 className="rainbow-text">hi</h1>
       <audio controls>
-        <source src="/cocaine.mp3" type="audio/mpeg" />
+        <source src="/cocaine.mp3" type="audio/mpeg"/>
       </audio>
-    </>
+      <i className="iconVolume bx bx-tada bx-flip-vertical bxs-volume-full" id="iconVolume" onclick="toggleMute();"></i
+    </> 
   )
 }
